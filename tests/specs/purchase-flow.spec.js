9456893fc82e8b515129dbe3f83f108d543c42e0
octavianChiriches <o.chiriches@outlook.com>
2025-11-23 13:01:55 +0200

    not POM test commented

--- tests/specs/purchase-flow.spec.js
+++ tests/specs/purchase-flow.spec.js
@@ -1,29 +1,29 @@
-import { test, expect } from '@playwright/test';
-import { InventoryPage } from '../pages/inventory.page';
+// import { test, expect } from '@playwright/test';
+// import { InventoryPage } from '../pages/inventory.page';
 
-test('purchase flow', async ({ page }) => {
+// test('purchase flow', async ({ page }) => {
     
-    const today = new Date().toLocaleDateString('en-GB');
-    const inventoryPage = new InventoryPage(page); 
+//     const today = new Date().toLocaleDateString('en-GB');
+//     const inventoryPage = new InventoryPage(page); 
 
     
-    await page.goto('/');
+//     await page.goto('/');
 
-    await expect(page).toHaveTitle(/Playground/);
-    await page.getByRole('link', { name: 'STORE' }).click();
-    await expect(page.getByTestId('instructions-title')).toBeVisible();
-    await page.getByTestId('store-tab-inventory').click();
-    inventoryPage.createInventoryProduct('Product 1', '22', '3'); 
-    await page.getByTestId('store-tab-catalog').click();
-    await expect(page.getByTestId('catalog-title')).toBeVisible();
-    await page.getByTestId('catalog-item-add-button-0').click();
-    await page.getByTestId('store-tab-cart').click();
-    await expect(page.getByTestId('cart-title')).toBeVisible();
-    await page.getByTestId('cart-go-to-payment').click();
-    await expect(page.getByTestId('payment-title')).toBeVisible();
-    await page.getByTestId('payment-method-input-MBWay').click();
-    await page.getByTestId('payment-confirm-button').click();
-    await expect(page.getByTestId('orders-title')).toBeVisible();
-    await expect(page.getByTestId('order-date-0')).toContainText(/Date: \d{1,2}\/\d{1,2}\/\d{4}/);
+//     await expect(page).toHaveTitle(/Playground/);
+//     await page.getByRole('link', { name: 'STORE' }).click();
+//     await expect(page.getByTestId('instructions-title')).toBeVisible();
+//     await page.getByTestId('store-tab-inventory').click();
+//     inventoryPage.createInventoryProduct('Product 1', '22', '3'); 
+//     await page.getByTestId('store-tab-catalog').click();
+//     await expect(page.getByTestId('catalog-title')).toBeVisible();
+//     await page.getByTestId('catalog-item-add-button-0').click();
+//     await page.getByTestId('store-tab-cart').click();
+//     await expect(page.getByTestId('cart-title')).toBeVisible();
+//     await page.getByTestId('cart-go-to-payment').click();
+//     await expect(page.getByTestId('payment-title')).toBeVisible();
+//     await page.getByTestId('payment-method-input-MBWay').click();
+//     await page.getByTestId('payment-confirm-button').click();
+//     await expect(page.getByTestId('orders-title')).toBeVisible();
+//     await expect(page.getByTestId('order-date-0')).toContainText(/Date: \d{1,2}\/\d{1,2}\/\d{4}/);
 
-});
+// });
